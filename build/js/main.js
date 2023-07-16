f92e3985f9f7a1d4b63e683910b23bb5e0153a41
Hopeson <hopesonogaga@gmail.com>
2023-07-16 03:51:55 +0100

    js func

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,14 @@
-console.log('hello')
+const card = document.querySelector(".live_card");
+const connect_acc = document.querySelector(".connect_acct");
+const border_width = document.querySelector(".border_width");
+
+
+
+
+
+card.classList.add('hidden')
+connect_acc.addEventListener("click", function () {
+    
+  border_width.style.height = '28%';
+  card.classList.toggle('hidden')
+});
